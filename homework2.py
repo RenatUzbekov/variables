57efc14a02821a9e81c3c1d0d225d7b0cfb4972a
RenatUzbekov <renat85m@mail.ru>
2024-07-25 02:25:14 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Number_of_completed_HW = 12
+Number_of_hours_spent = 1.5
+Course_name = "Python"
+Time_per_task = (float(Number_of_hours_spent)/int(Number_of_completed_HW))
+print('Курс:',Course_name,', всего задач:',Number_of_completed_HW, ', затрачено часов:',Number_of_hours_spent,', среднее время выполнения',Time_per_task)
